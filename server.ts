929412b04bda72968c32dbd984a1538975efdf4f
00Peterson1 <161578371+00Peterson1@users.noreply.github.com>
2026-04-23 16:40:46 +0300

    Server.ts file added

---
+++ server.ts
@@ -0,0 +1,16 @@
+const http = require("node:http");
+const port = 4000
+const server = http.createServer((req : any,res: any) =>
+{
+    res.writeHead(200, {
+        "content-length": 300,
+        "content-type": "text/html"
+    });
+
+    res.write("<h1>My name is Peterson Mwaura, a Backend Engineer...yaaahh👌😁</h1>");
+    res.end();
+});
+
+server.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
