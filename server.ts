bc8ee94c57d4cdf8295bb31266d5a9d2751fbb53
00Peterson1 <161578371+00Peterson1@users.noreply.github.com>
2026-04-24 15:47:46 +0300

    Added the files authcontroller, auth, userService and user

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
 const http = require("node:http");
 const port = 4000
-const server = http.createServer((req : any,res: any) =>
+const server = http.createServer((req:any,res:any) =>
 {
     res.writeHead(200, {
         "content-length": 300,
         "content-type": "text/html"
-    });
+    })
 
     res.write("<h1>My name is Peterson Mwaura, a Backend Engineer...yaaahh👌😁</h1>");
     res.end();
