0fa960a7c4617ddd31d73b37e2ee038e3b503b11
00Peterson1 <161578371+00Peterson1@users.noreply.github.com>
2026-04-29 12:34:51 +0300

    Fixed the ssl for the RAILWAY Postgres

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool(
  {
-   connectionString: process.env.DATABASE_URL, ssl:process.env.NODE_ENV === "production" ? {rejectUnathorized: false} : false
+   connectionString: process.env.DATABASE_URL, ssl: {rejectUnathorized: false}
  }
 )
 
