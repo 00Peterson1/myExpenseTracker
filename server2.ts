4c32249091e8b13e517989c2cea76f6143fd701a
00Peterson1 <161578371+00Peterson1@users.noreply.github.com>
2026-05-17 23:06:38 +0300

    Server2 Running

--- server2.ts
+++ server2.ts
@@ -1,19 +1,26 @@
 // HTTP module is a node module solution for creating http servers and clients.
 // It is a built-in module that provides an easy way to create a web server and handle HTTP requests and responses.
 const http = require("node:http");
-console.log(http)
 
 const port = 4000;
 
 http.createServer((req:any, res:any) =>{
+   
+if(req.url === "/about"){
     res.writeHead(200, {
         "content-length": 300,
         "content-type": "text/html"
     }),
     res.write("<h1>My name is Peterson Mwaura, a Backend Engineer...yaaahh👌😁</h1>");
     res.end();
-})
-
-http.listen(port, () => {
+}
+else if(req.method === "GET" && req.url === "/"){
+    res.writeHead(200, {
+        "content-length": 300,
+        "content-type": "text/html"
+    }),
+    res.write("<h1>Contact me on my email: hello@petersonlabs.dev</h1>");
+    res.end();
+}}).listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+})
